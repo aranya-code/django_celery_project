0095c1bca435c6994db931417821fd44e66de1a9
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-02-20 23:32:21 +0530

    setup

--- dcelery/settings.py
+++ dcelery/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
+CELERY_BROKER_BACKEND = os.environ.get('CELERY_BACKEND', 'redis://redis:6379/0')
